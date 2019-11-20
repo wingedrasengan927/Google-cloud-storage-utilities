7c87db2fffc7bdb0744286c5f2be9bb03eadf875
neeraj <neerajkrishna.macharla@quantiphi.com>
2019-11-20 19:09:29 +0530

    update

--- gcp_utils.py
+++ gcp_utils.py
@@ -1,7 +1,8 @@
 from google.cloud import storage
+import argparse
 import os
 
-def download_folder_from_bucket(bucket_name, folder_name, destination_folder_name, service_key_path):
+def download_folder_from_bucket(bucket_name, folder_name, local_folder_name, service_key_path):
     """Downloads a folder from the bucket."""
     storage_client = storage.Client.from_service_account_json(service_key_path)
     bucket = storage_client.get_bucket(bucket_name)
@@ -11,7 +12,7 @@ def download_folder_from_bucket(bucket_name, folder_name, destination_folder_nam
         if '.' in file.name:
             filename = file.name.split('/')[-1]
             # note that the dir should exist before downloading to that dir
-            file.download_to_filename(destination_folder_name +"/"+filename)
+            file.download_to_filename(local_folder_name +"/"+filename)
 
 def upload_files_into_bucket(bucket_name, bucketFolder, localFolder, service_key_path):
     """Upload files to GCP bucket."""
@@ -23,5 +24,21 @@ def upload_files_into_bucket(bucket_name, bucketFolder, localFolder, service_key
         blob = bucket.blob(bucketFolder + "/" + file)
         blob.upload_from_filename(localFile)
 
-# download_folder_from_bucket("temp-bucket-for-lab", "input_images", "images_from_gcp", "/home/user/Downloads/balmy-flash-255917-08b1d111e645.json")
-# upload_files_into_bucket("temp-bucket-for-lab", "input_images_2", "images_from_gcp", "/home/user/Downloads/balmy-flash-255917-08b1d111e645.json")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", action="store_true", default=False, help="download files from a folder in a google cloud bucket")
+    parser.add_argument("--service_account", default=None, type=str, help="path to the service account json file")
+    parser.add_argument("--bucket_name", default=None, type=str, help="name of the google storage bucket")
+    parser.add_argument("--bucket_folder", default=None, type=str, help="name of the folder inside the bucket whose files you want to download")
+    parser.add_argument("--local_folder", default=None, type=str, help="the folder in your machine you want to download or upload the files")
+    parser.add_argument("-u", action="store_true", default=False, help="upload files into a folder inside a bucker")
+
+    args = parser.parse_args()
+
+    if args.d:
+        download_folder_from_bucket(args.bucket_name, args.bucket_folder, args.local_folder, args.service_account)
+    elif args.u:
+        upload_files_into_bucket(args.bucket_name, args.bucket_folder, args.local_folder, args.service_account)
+
+if __name__ == "__main__":
+    main()
